5613fcba4c7c05d65a4029727d21fb47341c9a8a
codeefather <emogutoff@yandex.ru>
2023-12-08 19:12:55 +0400

    Create models

--- src/main/java/com/taskmanager/models/Task.java
+++ src/main/java/com/taskmanager/models/Task.java
@@ -1,4 +1,84 @@
 package com.taskmanager.models;
 
 public class Task {
+    private long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private User author;
+    private User assignee;
+
+    public Task() {
+
+    }
+
+    public Task(String title, String description, String status, String priority, User author, User assignee) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.author = author;
+        this.assignee = assignee;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Заголовок
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Описание
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    // Автор
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
 }
