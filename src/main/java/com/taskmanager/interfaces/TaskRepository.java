d5308aad7dc3bdffcf1f54f9f4ddacf329e6fe85
codeefather <emogutoff@yandex.ru>
2023-12-08 19:15:18 +0400

    Create interfaces

--- src/main/java/com/taskmanager/interfaces/TaskRepository.java
+++ src/main/java/com/taskmanager/interfaces/TaskRepository.java
@@ -1,4 +1,17 @@
 package com.taskmanager.interfaces;
 
+import com.taskmanager.models.Task;
+
+import java.util.List;
+
 public interface TaskRepository {
+    Task findById(long id);
+
+    List<Task> findAll();
+
+    void save(Task task);
+
+    void update(Task task);
+
+    void deleteById(long id);
 }
--- src/main/java/com/taskmanager/interfaces/UserRepository.java
+++ src/main/java/com/taskmanager/interfaces/UserRepository.java
@@ -1,4 +1,17 @@
 package com.taskmanager.interfaces;
 
+import com.taskmanager.models.User;
+
+import java.util.List;
+
 public interface UserRepository {
+    User findById(long id);
+
+    List<User> findAll();
+
+    void save(User user);
+
+    void update(User user);
+
+    void deleteById(long id);
 }
