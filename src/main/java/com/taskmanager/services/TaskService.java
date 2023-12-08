91ead31ae0c7f4442cbfe57cf85da964620f3796
codeefather <emogutoff@yandex.ru>
2023-12-08 19:19:26 +0400

    Create services

--- src/main/java/com/taskmanager/services/TaskService.java
+++ src/main/java/com/taskmanager/services/TaskService.java
@@ -1,4 +1,34 @@
 package com.taskmanager.services;
 
-public class TaskService {
+import com.taskmanager.interfaces.TaskRepository;
+import com.taskmanager.models.Task;
+
+import java.util.List;
+
+public class TaskService{
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task findTaskById(long id) {
+        return taskRepository.findById(id);
+    }
+
+    public List<Task> findAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void updateTask(Task task) {
+        taskRepository.update(task);
+    }
+
+    public void deleteTaskById(long id) {
+        taskRepository.deleteById(id);
+    }
 }
--- src/main/java/com/taskmanager/services/UserService.java
+++ src/main/java/com/taskmanager/services/UserService.java
@@ -1,4 +1,34 @@
 package com.taskmanager.services;
 
+import com.taskmanager.interfaces.UserRepository;
+import com.taskmanager.models.User;
+
+import java.util.List;
+
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUserById(long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void updateUser(User user) {
+        userRepository.update(user);
+    }
+
+    public void deleteUserById(long id) {
+        userRepository.deleteById(id);
+    }
 }
